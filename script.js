94e1227c729cc43a4ebed18727c55dd9e8dcdcfa
Mubashir Ali <mubashir@DebianTower.SUPAHOT500.net>
2022-11-21 20:41:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const DEFAULT_COLOUR = '#504E4E'
+const DEFAULT_SIZE = 20
+
+let currentColour = DEFAULT_COLOUR
+let currentSize = DEFAULT_SIZE
+
+function setCurrentColour(newColour) {
+    currentColour = newColour
+}
+
+function setCurrentSize(newSize) {
+    currentSize = newSize
+}
+
+const colourSlider = document.getElementById('colourSlider')
+const clear = document.getElementById('clearBtn')
+const sizeValue = document.getElementById('sizeValue')
+const sizeSlider = document.getElementById('sizeSlider')
+const grid = document.getElementById('grid')
+
+colourSlider.oninput = (e) => setCurrentColour(e.target.value)
+clear.onclick = () => reloadGrid()
+sizeSlider.onmousemove = (e) => updateDisplaySize(e.target.value)
+sizeSlider.onchange = (e) => updateGridSize(e.target.value)
+
+function updateGridSize(newSize) {
+    setCurrentSize(newSize)
+    updateDisplaySize(newSize)
+    reloadGrid()
+}
+
+function updateDisplaySize(newSize) {
+    sizeValue.innerHTML = `${newSize} x ${newSize}`
+}
+
+function reloadGrid() {
+    clearGrid()
+    createGrid(currentSize)
+}
+
+function clearGrid() {
+    grid.innerHTML = ''
+}
+
+function createGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i=0; i<size*size; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add("grid-element")
+        gridElement.addEventListener('mouseover', changeColour)
+        gridElement.addEventListener('mousedown', changeColour)
+        grid.appendChild(gridElement)
+    }
+}
+
+function changeColour(e) {
+    if (e.type === 'mouseover' && 'mousedown') {
+        e.style.backgroundColor = currentColour
+    }
+}
+
+window.onload = () => {
+    createGrid(DEFAULT_SIZE)
+}
